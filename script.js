cb4f35ae3470ca5a0b9196a061ab7b13c155d21c
denyskh4991 <denyskh4991@gmail.com>
2024-08-15 22:06:08 +0200

    Finished storage and added function for writing new lines

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
 
+function showNotes () {
+    notesContainer.innerHTML = localStorage.getItem("notes");
+}
 
+showNotes();
 
 function updateStorage(){
     localStorage.setItem("notes", notesContainer.innerHTML);
@@ -29,5 +33,12 @@ notesContainer.addEventListener("click", function (e){
                 updateStorage();
             }
         })
+    }
+})
+
+document.addEventListener("keydown", event =>{
+    if (event.key === "Enter") {
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
     }
 })
